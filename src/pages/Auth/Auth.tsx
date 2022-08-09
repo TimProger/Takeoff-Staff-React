da7a7fc0c7dac42a08a812d7f864a8b88b9185d8
TimProger <akylverentimofei@mail.ru>
2022-08-09 23:51:33 +0300

    подкорректировал стили страницы авторизации

--- src/pages/Auth/Auth.tsx
+++ src/pages/Auth/Auth.tsx
@@ -1,17 +1,18 @@
 import React from 'react';
 import AuthForm from "../../components/AuthForm/AuthForm";
 import Typography from "@mui/material/Typography";
+import {Card} from "@mui/material";
 
 const Auth = () => {
 
     return (
-        <div>
-            <Typography variant="h4" component="div" gutterBottom>
+        <Card sx={{ minWidth: 375, padding: 2 }}>
+            <Typography variant="h4" component="div" style={{ textAlign: 'center' }}>
                 Welcome to <span>ContactMe</span>
             </Typography>
-            <p>Please login</p>
+            <p style={{ textAlign: 'center' }}>Please login</p>
             <AuthForm />
-        </div>
+        </Card>
     );
 };
 
